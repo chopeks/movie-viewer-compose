93b2f920f9722455f875ab508d09eb1abdd750bf
chopeks <37939738+chopeks@users.noreply.github.com>
2026-03-08 23:13:38 +0100

    fixed di

--- core/database-jvm/src/desktopMain/kotlin/pl/chopeks/core/database/_di.kt
+++ core/database-jvm/src/desktopMain/kotlin/pl/chopeks/core/database/_di.kt
@@ -5,8 +5,12 @@ import org.kodein.di.bindProvider
 import org.kodein.di.bindSingleton
 import org.kodein.di.instance
 import pl.chopeks.core.database.datasource.ActorLocalDataSource
+import pl.chopeks.core.database.datasource.DirectoriesLocalDataSource
+import pl.chopeks.core.database.datasource.SettingsLocalDataSource
 
 val databaseModule = DI.Module("databaseModule") {
 	bindSingleton { DatabaseHelper.connect() }
 	bindProvider { ActorLocalDataSource(instance()) }
+	bindProvider { SettingsLocalDataSource() }
+	bindProvider { DirectoriesLocalDataSource(instance()) }
 }
