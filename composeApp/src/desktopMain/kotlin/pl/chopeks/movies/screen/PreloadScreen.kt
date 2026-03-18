9fe768c6d4272b1ad52e25240f3870062b233cae
chopeks <37939738+chopeks@users.noreply.github.com>
2026-03-18 16:47:57 +0100

    added automatic scrolling to loading screen log

--- composeApp/src/desktopMain/kotlin/pl/chopeks/movies/screen/PreloadScreen.kt
+++ composeApp/src/desktopMain/kotlin/pl/chopeks/movies/screen/PreloadScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,28 +14,36 @@ import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.kodein.rememberScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
-import pl.chopeks.movies.tasks.DuplicatesSearchTask
 
 class PreloadScreen : Screen {
-  @Composable
-  override fun Content() {
-    val screenModel = rememberScreenModel<PreloadScreenModel>()
-    val navigator = LocalNavigator.current
-    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
-      LazyColumn(Modifier.fillMaxSize()) {
-        items(screenModel.events) {
-          Text(it, color = Color.Gray)
-        }
-      }
-    }
+	@Composable
+	override fun Content() {
+		val screenModel = rememberScreenModel<PreloadScreenModel>()
+		val listState = rememberLazyListState()
+		val navigator = LocalNavigator.current
 
-    LaunchedEffect(Unit) {
-      screenModel.init()
-    }
-    LaunchedEffect(screenModel.isDone) {
-      if (screenModel.isDone) {
-        navigator?.replace(ActorsScreen())
-      }
-    }
-  }
+		Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+			LazyColumn(state = listState, modifier = Modifier.fillMaxSize()) {
+				items(screenModel.events) {
+					Text(it, color = Color.Gray)
+				}
+			}
+		}
+
+		LaunchedEffect(screenModel.events.size) {
+			if (screenModel.events.isNotEmpty()) {
+				listState.animateScrollToItem(screenModel.events.size - 1)
+			}
+		}
+
+		LaunchedEffect(Unit) {
+			screenModel.init()
+		}
+
+		LaunchedEffect(screenModel.isDone) {
+			if (screenModel.isDone) {
+				navigator?.replace(ActorsScreen())
+			}
+		}
+	}
 }
